b408e38448cecb98a488bf86ea53d34852131c7d
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-01-29 11:45:33 -0700

    Practice.py code

--- variables/notes.py
+++ variables/notes.py
@@ -1,3 +1,14 @@
 # Robert Nelson, Variable notes
 
-print("It worked!")
+name = "Robert"
+
+subject = "CSP"
+
+subject = input("What's your favorite subject: ")
+
+small_number = 4
+
+name = input("What is your name: ")
+
+print(name, "really likes", subject)
+print(subject)
---
+++ variables/practice.py
@@ -0,0 +1,10 @@
+my_name = "Robert"
+number = "4"
+bigger_number = "500"
+breakfast = "Rice Krispies"
+fav_color = "Blue"
+school_name = "UCAS"
+year = 2025
+eye_color = "Brown"
+age = 15
+fav_subject = "Leadership"
