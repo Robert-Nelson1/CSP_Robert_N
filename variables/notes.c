28f4acf842868f3c80b7750e8ed052317656d0d7
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-01-30 11:50:44 -0700

    I did c notes

---
+++ variables/notes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+char name[] = "Robert";
+int age = 15;
+float pi = 3.14;
+
+
+    printf("Hello I am %s. I am %d years old. I like the number %f \n", name, age, pi);
+    printf("%d\n", age);
+    printf("%f\n", pi);
+    return 0;
+}
+
