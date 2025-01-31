ff5233c3d31b8c7fc2b4efdea58f4b809e5914c1
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-01-31 11:50:17 -0700

    I programed

--- variables/first_program.c
+++ variables/first_program.c
@@ -3,7 +3,15 @@
 int main(void){
 
     printf("Hello World");
+    int age;
+
+// Ask the user to type a number
+printf("How old are you: \n");
+
+// Get and save the number the user types
+scanf("%d", age);
+
+// Output the number the user typed
+    printf("You are:\n", age);
     return 0;
 }
-
-char name[] = scanf("What is your name:");
