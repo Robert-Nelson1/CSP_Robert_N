7fa7c90d6f9df37ab0f44b5d27e7f651f6f66bf2
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-01-31 21:46:43 -0700

    Update firstProgram.C
    I did it

--- variables/firstProgram.C
+++ variables/firstProgram.C
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main (void){
+    char Utah[] = "Robert";
+    printf("What is the capital of Utah: \n");
+    scanf ("%s", Utah);
+    printf("Hello, %s!", Utah);
+    
+    
+    return 0;
+}
