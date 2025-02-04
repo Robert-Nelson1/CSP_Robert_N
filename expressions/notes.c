ea514ee15e4030b9746a82352508b215db5c5b52
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-02-04 11:18:43 -0700

    I did notes in the expressions folder

--- expressions/notes.c
+++ expressions/notes.c
@@ -3,9 +3,12 @@
 #include <math.h>
 float equation = 5*7+11/(5-pow(2,2));
 float x = pow(2,2);
+float pi = 3.1415926;
 // + - * /
 int main(void){
-
+    //num = 4;
+    //printf("%d\n", num);
+    printf("%f\n", pi);
    // printf("%f", equation);
     return 0;
 }
--- expressions/notes.py
+++ expressions/notes.py
@@ -7,7 +7,7 @@ print(70/8)
 print(70**8)
 print(70//8) #integer division return just the whole number
 print(70%8) #Modulo/modulus/Mod return the remainder
-"""
+
 num = 4
 
 print(num)
@@ -31,3 +31,7 @@ print(num)
 num **= 1
 
 print(num)
+"""
+pi = 3.14159
+print(round(pi))
+print(f"{pi:.4f}")
