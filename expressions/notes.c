30faae1db9d6d45b057327f3db298bf43b3185ca
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-02-05 11:49:19 -0700

    I did practice.py and practice.c in expressions.

--- expressions/notes.c
+++ expressions/notes.c
@@ -4,12 +4,15 @@
 float equation = 5*7+11/(5-pow(2,2));
 float x = pow(2,2);
 float pi = 3.1415926;
+float expression = 5*(int)pow(7, 2)/4;
 // + - * /
 int main(void){
     //num = 4;
     //printf("%d\n", num);
     printf("%f\n", pi);
    // printf("%f", equation);
+   printf("%f", expression);
+   
     return 0;
 }
 
---
+++ expressions/practice.c
@@ -0,0 +1,23 @@
+// Robert Nelson, Expressions Practice - C
+#include <stdio.h>
+#include <math.h>
+int one = 7-24/8*4+6;
+int two = 18/3-7+2*5;
+int three = 6*4/12+72/8-9;
+int four = (17-6/2)+4*3;
+int five = -2*(1*4-2/2)+(6+2-3);
+int six = -1*((3-4*7)/5)-2*24/6;
+int seven = (3*(int)pow(5, 2)/15)-(5-(int)pow(2, 2));
+// not done // int eight = (int)pow(1, 4)*
+
+int main(void){
+    printf("%d\n", one);
+    printf("%d\n", two);
+    printf("%d\n", three);
+    printf("%d\n", four);
+    printf("%d\n", five);
+    printf("%d\n", six);
+    printf("%d\n", seven);
+    return 0;
+}
+// char name[20];
