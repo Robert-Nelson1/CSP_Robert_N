b7d71e6985796059f748689b84ba5502994af1af
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-02-11 11:15:59 -0700

    edited notes.py

--- strings/notes.py
+++ strings/notes.py
@@ -2,10 +2,17 @@
 
 #String: data type that holds any info surrounded by quotation marks ""
 note ="Vienna's class"
-name = input("What is your first name?\n").strip().lower().capitalize()
+#name = input("What is your first name?\n").strip().lower().capitalize()
 
-print(f"Hello {name}, welcome to my program!")
+#print(f"Hello {name}, welcome to my program!")
 
 
-sentence = "Hola"
+sentence = "The quick brown fox jumps over the lazy dog"
+print(len(sentence))
+print(sentence[4])
+print(sentence.find("brown"))
 
+start = sentence.find("brown")
+length = len("brown fox")
+
+print(sentence[10:19])
