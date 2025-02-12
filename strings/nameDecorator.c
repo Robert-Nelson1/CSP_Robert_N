8ab4fa0005df0c17f74848607abc82a8cc4ba6ae
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-02-12 11:47:48 -0700

    I got rid of some commented out stuff

--- strings/nameDecorator.c
+++ strings/nameDecorator.c
@@ -9,13 +9,8 @@ int main(void){
     scanf("%s", name);
     char smiley[] = "(: (: (: ";
     char moreSmiley[] = " :) :) :)";
-    //printf("%s", smiley);
     strcat(smiley, name);
-    // printf("%s", smiley);
     strcat(smiley, moreSmiley); // can only concatanate one thing at a time
-    // printf("%s", smiley);
-
-
     printf("Hello %s, welcome to my program!", smiley);
 
     return 0;
