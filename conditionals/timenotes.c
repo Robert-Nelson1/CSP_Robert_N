721462176a9fbbc96903d67eb8d7cb22e63d22be
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-02-28 11:07:26 -0700

    I created time.c

--- conditionals/timenotes.c
+++ conditionals/timenotes.c
@@ -7,17 +7,23 @@ int main(void){
     time_t seconds;
 
     seconds = time(NULL);
+    //printf("Seconds since Jan 1, 1970 = %d", seconds);
+    // Current time
     time_t rawtime;
     struct tm * timeinfo;
 
     time(&rawtime);
     timeinfo = localtime(&rawtime);
-    
+    //printf("Current time and date is %s", asctime(timeinfo));
+
+    //current hour
     time_t now = time(NULL);
 
     struct tm * tm_struct = localtime(&now);
     int hour = tm_struct->tm_hour;
     printf("%d\n", hour);
+
+
 
     if(hour >=0 && hour <=12){
         printf("Good morning\n");
@@ -31,3 +37,12 @@ int main(void){
 
     return 0;
 }
+
+
+// #< less than
+// #> greater than
+// #<= less than or equal to
+// #>= greater than or equal to
+// #== Equal to
+// #=== *Doesn't exist in Python*
+// #! Not
