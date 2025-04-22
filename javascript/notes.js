4fabdd8cd9c2995be8da496be9ef92837338cd15
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-04-22 11:49:33 -0600

    I made some more assignments

---
+++ javascript/notes.js
@@ -0,0 +1,29 @@
+let images =["https://ac.blooket.com/marketassets/blooks/yeti.svg", "https://ac.blooket.com/marketassets/blooks/rabbit.svg"]
+let counter = 0
+function change(){
+    if(counter < images.length){
+        document.getElementById("img").src = images[counter]
+        counter += 1
+    }else{
+        counter = 0
+        document.getElementById("img").src = images[counter]
+    }
+    
+}
+
+function hello(){
+    document.getElementById("title").style.color = "orange"
+    document.getElementById("title").innerHTML = "orange"
+}
+
+function hover(){
+    document.getElementById("img").src = "https://ac.blooket.com/marketassets/blooks/whiterabbit.svg"
+}
+
+function leave(){
+    document.getElementById("img").src = "https://ac.blooket.com/marketassets/blooks/yeti.svg"
+}
+
+function hidden(){
+    document.getElementById("meme").style.display = "block"
+}
