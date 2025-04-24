b2afa1dfed84e1c0453ac977ce0cd3ad5ed7f3f7
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-04-24 11:48:50 -0600

    I edited the choice page

---
+++ HTML_Advanced/choice_page.js
@@ -0,0 +1,13 @@
+function hover(){
+    document.getElementById("img").src = "https://ac.blooket.com/marketassets/blooks/springrabbit.svg"
+}
+
+function leave(){
+    document.getElementById("img").src = "https://ac.blooket.com/marketassets/blooks/whiterabbit.svg"
+}
+
+
+function hello(){
+    //document.getElementById("click_me").style.color = "darkgreen"
+    document.getElementById("click_me").innerHTML = "My personal experience with rabbits is "
+}
