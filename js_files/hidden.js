c45691ce496f611557f0278bd6b90093c38d2f3b
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-04-24 18:41:20 -0600

    I made my hidden web page better

--- js_files/hidden.js
+++ js_files/hidden.js
@@ -1,4 +1,4 @@
-
-function hover(){
-    document.getElementById("img").src = "https://ac.blooket.com/marketassets/blooks/whiterabbit.svg"
+function hover() {
+    var imgElement = document.getElementById("img");
+    imgElement.style.display = "block"; // Reveal the rabbit image when hovering over the hover area
 }
