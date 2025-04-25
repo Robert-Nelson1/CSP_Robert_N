20c1057c62dead17bd3820fb0d9fa3e690da1bd8
Robert-Nelson1 <robert.nelson@ucas-edu.net>
2025-04-25 17:42:29 -0600

    I finished my first webpage

--- HTML_Advanced/choice_page.js
+++ HTML_Advanced/choice_page.js
@@ -6,11 +6,15 @@ function leave(){
     document.getElementById("img").src = "https://ac.blooket.com/marketassets/blooks/whiterabbit.svg"
 }
 
-
 function hello(){
-    //document.getElementById("click_me").style.color = "darkgreen"
-    document.getElementById("click_me").innerHTML = "My two lovely pets, Oreo and Precious, have made my days go by fast, as spending time with them is the best part of my day. Oreo, is always ready to entertain anyone with his playful hops and curious nose twitches. He always manages to make me laugh with all his sneaky behavior. This is especially true when he tries to steal snacks he's not supposed to get. Precious, is his opposite who is much gentler and more affectionate. Her favorite thing is to cuddle up next to me and let me pet her soft fur. Watching them interact and chase each other around, or just relax in their favorite corner brings a sense of joy that I didn't even realize I needed. No matter how stressful my day is, these two, always manage to lift my spirits up and remind me to slow down and appreciate the little things.";
-   /*<img src="https://www.ucas-edu.net/assets/images/ucas-logo.svg" alt="My pet rabbits" style="width: 300px; border-radius: 10px; margin-top: 10px;">;*/
-   document.getElementById("meme").style.display = "block"
+    const image = document.getElementById("rabbit");
+    const text = document.getElementById("story");
+    if (text.style.display === "none") {
+        text.innerHTML = "My two lovely pets, Oreo and Precious, have made my days go by fast, as spending time with them is the best part of my day. Oreo, is always ready to entertain anyone with his playful hops and curious nose twitches. He always manages to make me laugh with all his sneaky behavior. This is especially true when he tries to steal snacks he's not supposed to get. Precious, is his opposite who is much gentler and more affectionate. Her favorite thing is to cuddle up next to me and let me pet her soft fur. Watching them interact and chase each other around, or just relax in their favorite corner brings a sense of joy that I didn't even realize I needed. No matter how stressful my day is, these two, always manage to lift my spirits up and remind me to slow down and appreciate the little things.";
+        image.style.display = "block";
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+        image.style.display = "none";
+    }
 }
-
